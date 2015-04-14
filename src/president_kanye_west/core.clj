72f1f3b5800157286fc12fac99d717c020960ce7
Paul Lambert <lambertington@gmail.com>
2015-04-14 17:12:48 -0500

    initial commit

---
+++ src/president_kanye_west/core.clj
@@ -0,0 +1,6 @@
+(ns president-kanye-west.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/president_kanye_west/core_test.clj
@@ -0,0 +1,7 @@
+(ns president-kanye-west.core-test
+  (:require [clojure.test :refer :all]
+            [president-kanye-west.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
